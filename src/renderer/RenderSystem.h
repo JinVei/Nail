c6089d1081dc92b53cab86308aaac5a6df155ef6
Jinvei <627327116@qq.com>
2020-08-02 22:30:30 +0800

    update

--- src/renderer/RenderSystem.h
+++ src/renderer/RenderSystem.h
@@ -6,16 +6,14 @@
 namespace nail {
     class RenderSystem {
         friend class nail::Context;
-
-        void setRenderVertexBufferFactory(ref<RenderVertexBufferFactory> factory) {
-            _vertex_buffer_factory = factory;
-        }
-
     private:
         RenderSystem() {};
         static ref<RenderSystem> _singleton;
         ref<RenderVertexBufferFactory> _vertex_buffer_factory = nullptr;
     public:
+        void setRenderVertexBufferFactory(ref<RenderVertexBufferFactory> factory) {
+            _vertex_buffer_factory = factory;
+        }
         static ref<RenderSystem> getSingleton() {
             return _singleton;
         }
