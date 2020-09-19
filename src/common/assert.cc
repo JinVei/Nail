9cfbe3a60961dc62bdc5624e112d870c568884ba
Jinvei <627327116@qq.com>
2020-09-19 15:13:31 +0800

    implement assert_faild based on boost/stacktrace.hpp

--- src/common/assert.cc
+++ src/common/assert.cc
@@ -1,5 +1,6 @@
 #include "assert.h"
-#include "backtrace.h"
+//#include "backtrace.h"
+#include <boost/stacktrace.hpp>
 #include <iostream>
 namespace jv {
 inline namespace v1 {
@@ -9,8 +10,9 @@ inline namespace v1 {
     std::cerr << "thread id: " << (unsigned long long)pthread_self() << std::endl;
 #endif
     std::cerr << file << "\t" << line << "\t" << func << std::endl;
-    Backtrace bt;
-    bt.print(std::cerr);
+    //Backtrace bt;
+    //bt.print(std::cerr);
+    std::cerr << boost::stacktrace::stacktrace();
     std::cerr.flush();
     std::abort();
   }
